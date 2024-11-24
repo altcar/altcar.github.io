448b1409c47ad9c9bf7b6f461f9d2627e1d251d3
You <hi@example.com>
2024-11-24 09:42:55 +0000

    rss

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -2,8 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel/serverless';
+import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
+  integrations: [mdx()],
 });
